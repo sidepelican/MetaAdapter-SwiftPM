ad4d1fcf5357c6b3fd8b0bcddc9e55777fca3b3d
kenta okamura <side.junktown@gmail.com>
2025-01-12 11:35:35 +0900

    Add FBAudienceNetwork export

--- Package.swift
+++ Package.swift
@@ -6,22 +6,23 @@ let package = Package(
     name: "MetaAdapter-SwiftPM",
     platforms: [.iOS(.v12)],
     products: [
-        .library(name: "MetaAdapter", targets: ["MetaAdapter"]),
+        .library(name: "MetaAdapter", targets: ["MetaAdapterTarget"]),
+        .library(name: "FBAudienceNetwork", targets: ["FBAudienceNetwork"]),
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.11.0"),
     ],
     targets: [
         .target(
-            name: "MetaAdapter",
+            name: "MetaAdapterTarget",
             dependencies: [
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
-                "BinaryMetaAdapter",
+                "MetaAdapter",
             ],
             path: "MetaAdapter"
         ),
         .binaryTarget(
-            name: "BinaryMetaAdapter",
+            name: "MetaAdapter",
             url: "https://dl.google.com/googleadmobadssdk/mediation/ios/meta/MetaAdapter-6.16.0.0.zip",
             checksum: "71a13abb8057caa971cb863890fb5d74a252906b83b299082a26183dffbc1b7c"
         ),
